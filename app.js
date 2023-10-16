ef392c5ee636eff0874f2f2e98074e42ed800776
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2023-10-17 00:13:43 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const prevButton = document.getElementById("prev-btn");
+const nextButton = document.getElementById("next-btn");
+
+let currentSlideIndex = 0;
+
+
+function showSlide() {
+  slides.forEach((slide, index) => {
+    slide.style.display = index === currentSlideIndex ? "flex" : "none";
+  });
+}
+
+function prevSlide() {
+  currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+  showSlide();
+}
+
+function nextSlide() {
+  currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+  showSlide();
+}
+
+prevButton.addEventListener("click", prevSlide);
+nextButton.addEventListener("click", nextSlide);
+
+showSlide();
+
